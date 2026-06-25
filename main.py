3476b85cd95138caf714c774fbe0af20c5608ed9
Your Name <you@example.com>
2026-06-25 22:04:20 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+"""
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Abdudinho/P39Ulashov.git
+git push -u origin main
+"""
