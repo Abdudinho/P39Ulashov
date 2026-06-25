5557c8c22acd81c24814a1f18278092250611553
Your Name <you@example.com>
2026-06-25 22:26:13 +0500

    first commit

--- payment.py
+++ payment.py
@@ -39,3 +39,12 @@ async def confirm_handler(message: Message):
 
 
 print("Payment bu ")
+
+"""
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Abdudinho/payments.git
+git push -u origin main
+"""
